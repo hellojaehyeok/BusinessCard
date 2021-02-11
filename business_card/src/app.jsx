506c16139200734632b2e709ba351d1742a98363
Song jae hyeok <wakij6587@naver.com>
2021-02-11 22:47:28 +0900

    react 기본 불필요 파일 제거, 코드 정리
    react 기본 불필요 파일 제거, 코드 정리

---
+++ business_card/src/app.jsx
@@ -0,0 +1,13 @@
+import './app.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Hello World
+      </header>
+    </div>
+  );
+}
+
+export default App;
