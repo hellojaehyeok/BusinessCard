9460eb08ff4e4fa8f84b92e3050ab1f64e54ec75
Song jae hyeok <wakij6587@naver.com>
2021-02-16 22:56:13 +0900

    이미지 업로드 버튼 로딩 스피너 추가 및 추가 후 스타일 변경
    이미지 업로드 버튼 로딩 스피너 추가 및 추가 후 스타일 변경

--- business_card/src/components/editor/image_file_input/image_file_input.jsx
+++ business_card/src/components/editor/image_file_input/image_file_input.jsx
@@ -1,12 +1,16 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 import styles from './image_file_input.module.css';
 
 const ImageFileInput = ({imageUploader, name, onFileChange}) => {
 
+    const [loading, setLoading] = useState(false);
+
     const inputRef = useRef();
 
     const onChange = async e => {
+        setLoading(true);
         const uploaded = await imageUploader.upload(e.target.files[0]);
+        setLoading(false);
         onFileChange({
             name: uploaded.original_filename,
             url:uploaded.url,
@@ -18,10 +22,16 @@ const ImageFileInput = ({imageUploader, name, onFileChange}) => {
         inputRef.current.click();
     }
 
+
     return(
         <div className={styles.ImageButtonWrap}>
             <input className={styles.imageInput} onChange={onChange} ref={inputRef} type="file" accept="image/*" name="profileImg"/>
-            <button className={styles.button} onClick={onClickInput}>{name || "이미지 업로드"}</button>
+            {!loading && <button className={`${styles.button} ${name?styles.activeInput:""}`} onClick={onClickInput}>
+                {name || "이미지 업로드"}
+            </button>}
+            
+            {loading && <div className={styles.loading}></div>}
+            
         </div>
     );
 };
